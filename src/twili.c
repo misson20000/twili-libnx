eafb83f2456c614f10872dd40d3a17bd5293db9c
misson20000 <xenotoad@xenotoad.net>
2019-04-06 11:46:27 -0700

    update copyright year

--- src/twili.c
+++ src/twili.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2018, misson20000
+  Copyright (c) 2019 misson20000
 
   Permission to use, copy, modify, and/or distribute this software for any purpose
   with or without fee is hereby granted, provided that the above copyright notice
